665e141466f2d08156adee1311f12f7d72004225
Khrisefzm <khrisefzm@gmail.com>
2024-02-24 22:23:26 -0500

    add images and Software Engineering Career Track as only course

--- app/components/PricingCard/page.js
+++ app/components/PricingCard/page.js
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 import Icon from "../Icon/set/check";
-import '../../styles/pricingcard.css'
+import "../../styles/pricingcard.css";
 import Link from "next/link";
 
 const data_static = {
@@ -9,17 +9,14 @@ const data_static = {
   just: "Just",
   price: "$500 down",
   button: {
-      button_text: "Learn More",
-      button_link: "/",
-      button_color: "#312E81",
-      button_textcolor: "#e5e7eb"
-  }
+    button_text: "Learn More",
+    button_link: "/",
+    button_color: "#312E81",
+    button_textcolor: "#e5e7eb",
+  },
 };
 
-const PricingCard = ({
-  data
-}) => {
-
+const PricingCard = ({ data }) => {
   return (
     <>
       <div className="rounded-8 box-shadow-v2 m-auto flex border-v1 w-100 pricing-card">
@@ -34,15 +31,18 @@ const PricingCard = ({
           </div>
           <div>
             <ul className="list-style-none columns-2">
-              {
-                data?.bullets.map((bullet, index) => {
-                  return (
-                    <li key={index} className="row fs-14 fw-400">
-                      <Icon icon={data_static?.icon} width="24" style={{ margin: "0 15px 0 0" }} />
-                      {bullet}
-                    </li>)
-                })
-              }
+              {data?.bullets.map((bullet, index) => {
+                return (
+                  <li key={index} className="row fs-14 fw-400">
+                    <Icon
+                      icon={data_static?.icon}
+                      width="24"
+                      style={{ margin: "0 15px 0 0" }}
+                    />
+                    {bullet}
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </div>
@@ -51,9 +51,16 @@ const PricingCard = ({
           <span className="fs-18 fw-700 lh-24">{data_static?.just}</span>
           <h1 className="fs-48 fw-800 lh-48">{data_static?.price}</h1>
           <p className="fs-14 fs-500 lh-24">{data?.finance_msg}</p>
-          <Link href={data_static?.button.button_link}>
-            <div className="button fs-16 fw-600 lh-24 rounded-4"
-            >{data_static?.button.button_text}</div>
+          <Link
+            href={
+              data.button_link
+                ? data.button_link
+                : data_static.button.button_link
+            }
+          >
+            <div className="button fs-16 fw-600 lh-24 rounded-4">
+              {data_static?.button.button_text}
+            </div>
           </Link>
         </div>
       </div>
--- app/page.js
+++ app/page.js
@@ -1,70 +1,63 @@
-'use client';
-import { React, useEffect, useState } from 'react'
-import Image from 'next/image'
-import styles from './page.module.css'
-import PageHeader from './components/PageHeader/page'
-import TitleContainer from './components/TitleContainer/page';
-import Carousel from './components/Carousel/page';
-import PricingCard from './components/PricingCard/page';
-import Credentials from './components/Credentials/page'
-import Paths from './components/Paths/page';
-import TwoColumns from './components/TwoColumns/page';
-import data from './data/home.json'
-
+"use client";
+import { React, useEffect, useState } from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
+import PageHeader from "./components/PageHeader/page";
+import TitleContainer from "./components/TitleContainer/page";
+import Carousel from "./components/Carousel/page";
+import PricingCard from "./components/PricingCard/page";
+import Credentials from "./components/Credentials/page";
+import Paths from "./components/Paths/page";
+import TwoColumns from "./components/TwoColumns/page";
+import data from "./data/home.json";
 
 export default function Home() {
-
-  console.log(data?.pricing_info_variable)
+  console.log(data?.pricing_info_variable);
 
   return (
     <main className={styles.main}>
-
       <PageHeader data={data?.header} />
 
-      <TitleContainer 
-        data={data.title_container[0]}  
+      <TitleContainer
+        data={data.title_container[0]}
         classContainer="text-center m-auto gap-16 "
-        classHeading="fs-36 fw-700 lh-36 fs-sm-30 fw-sm-700" 
+        classHeading="fs-36 fw-700 lh-36 fs-sm-30 fw-sm-700"
         classParagraph="fs-16 fw-400 lh-28 color-gray-v2"
       />
 
       <Carousel data={data?.carousel} />
 
-      <TitleContainer 
-        data={data.title_container[1]} 
+      <TitleContainer
+        data={data.title_container[1]}
         classContainer="text-center m-auto gap-16 "
-        classHeading="fs-36 fw-700 lh-36 fs-sm-30 fw-sm-700" 
+        classHeading="fs-36 fw-700 lh-36 fs-sm-30 fw-sm-700"
         classParagraph="fs-16 fw-400 lh-28 color-gray-v2"
       />
 
-      {(data?.pricing_info_variable) && (
-        <div className='gap-24 m-auto column py-30 container maxwidth-1366'>
-
+      {data?.pricing_info_variable && (
+        <div className="gap-24 m-auto column py-50 maxwidth-1366">
           {data?.pricing_info_variable?.map((card, index) => {
-            return (
-              <PricingCard
-                key={index}
-                data={card}
-              />
-            )
+            if (card.active) return <PricingCard key={index} data={card} />;
+            else return <></>;
           })}
-
         </div>
       )}
 
       <Credentials />
 
-      <TitleContainer 
-        data={data.title_container[2]} 
+      <TitleContainer
+        data={data.title_container[2]}
         classContainer="text-center m-auto gap-16 "
-        classHeading="fs-36 fw-700 lh-36 fs-sm-30 fw-sm-700" 
+        classHeading="fs-36 fw-700 lh-36 fs-sm-30 fw-sm-700"
         classParagraph="fs-16 fw-400 lh-28 color-gray-v2"
       />
 
       <Paths />
 
-      <TwoColumns left={data?.two_columns?.left} right={data?.two_columns?.right} />
-
+      <TwoColumns
+        left={data?.two_columns?.left}
+        right={data?.two_columns?.right}
+      />
     </main>
-  )
+  );
 }
