5ac7e5087a7b08c843794ea7caa35a5c8dd02d58
gustavomm19 <gustavomm199@gmail.com>
2024-04-16 18:14:05 +0000

    add gtm key to env variables

--- app/pages/_document.jsx
+++ app/pages/_document.jsx
@@ -11,7 +11,7 @@ function Document() {
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer',''GTM-XXXXXX'');
+              })(window,document,'script','dataLayer',''${process.env.TAG_MANAGER_KEY}'');
             `,
           }}
         />
@@ -20,7 +20,7 @@ function Document() {
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-xxxx"
+              <iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.TAG_MANAGER_KEY}"
               height="0" width="0" style="display:none;visibility:hidden"></iframe>
             `,
           }}
